559b6f9f84dffe9e72edc686b7f25f91eee8052c
Cyril-Catoen <cyril.catoen@lapiscine.pro>
2025-04-14 15:12:53 +0200

    fonction construct

--- Reservation.model.php
+++ Reservation.model.php
@@ -19,24 +19,25 @@ class Reservation {
 	public $bookedAt;
 
 	public $cleaningOption;
+
+	// on automatise le traitement par la création d'une fonction construct
+	function __construct() {
+		$this->name = "Cyril Catoen";
+		$this->place = "Tokyo Sky Tower";
+		$this->startDate = new DateTime("25-04-15");
+		$this->endDate = new DateTime("25-06-16");
+		$this->cleaningOption = true;
+		$this->nightPrice = 500;
+
+		// Calcul du prix par nuit
+		$totalPrice = (($this->endDate->getTimestamp() - $this->startDate->getTimestamp()) / (3600 * 24) * $this->nightPrice) + 50;
+		$this->totalPrice = $totalPrice;
+		$this->bookedAt = new DateTime();
+		$this->status = "CART";
+			}
 }
 
 // On initialise la création de l'objet
 $reservation = new Reservation();
-
-// On déclare les valeurs des propriétés pour créer l'objet
-$reservation->name = "Cyril Catoen";
-$reservation->place = "Tokyo Sky Tower";
-$reservation->startDate = new DateTime("25-04-15");
-$reservation->endDate = new DateTime("25-06-16");
-$reservation->cleaningOption = true;
-$reservation->nightPrice = 500;
-
-// Calcul du prix par nuit
-$totalPrice = (($reservation->endDate->getTimestamp() - $reservation->startDate->getTimestamp()) / (3600 * 24) * $reservation->nightPrice) + 50;
-$reservation->totalPrice = $totalPrice;
-$reservation->bookedAt = new DateTime();
-$reservation->status = "CART";
-
 
 var_dump($reservation); 
