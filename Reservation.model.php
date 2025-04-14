f6a69d2404e8f2bbdbb77218206687034a17b945
Cyril-Catoen <cyril.catoen@lapiscine.pro>
2025-04-14 16:34:58 +0200

    __construct devient variable

--- Reservation.model.php
+++ Reservation.model.php
@@ -21,12 +21,13 @@ class Reservation {
 	public $cleaningOption;
 
 	// on automatise le traitement par la création d'une fonction construct
-	function __construct() {
-		$this->name = "Cyril Catoen";
-		$this->place = "Tokyo Sky Tower";
-		$this->startDate = new DateTime("25-04-15");
-		$this->endDate = new DateTime("25-06-16");
-		$this->cleaningOption = true;
+	function __construct($name, $place, $startDate, $endDate, $cleaningOption)  {
+		$this->name = $name;
+		$this->place = $place;
+		$this->startDate = $startDate;
+		$this->endDate = $endDate;
+		$this->cleaningOption = $cleaningOption;
+
 		$this->nightPrice = 500;
 
 		// Calcul du prix par nuit
@@ -37,7 +38,14 @@ class Reservation {
 			}
 }
 
+// On simule les données de la BDD
+$name = "Cyril Catoen";
+$place = "Tokyo Sky Tower";
+$start = new DateTime('2025-04-15');
+$end = new DateTime('2025-06-16');
+$cleaning = false;
+
 // On initialise la création de l'objet
-$reservation = new Reservation();
+$reservation = new Reservation($name , $place, $start, $end, $cleaning);
 
 var_dump($reservation); 
